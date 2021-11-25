f442e0a4c506cb999d891e4407678733b0a8a69d
Daiki Chibana <a2018112376@estudiantes.upsa.edu.bo>
2021-11-25 17:14:38 -0400

    Creacion del proyecto de consola.

---
+++ SolidConsolaEjemplos/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SolidConsolaEjemplos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
